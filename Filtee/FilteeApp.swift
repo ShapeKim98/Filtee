f0f65aa86adfd9faf02265cc076099f9297f7d0e
김도형 <shapekim98@gmail.com>
2025-05-11 13:39:03 +0900

    git init

---
+++ Filtee/FilteeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilteeApp.swift
+//  Filtee
+//
+//  Created by 김도형 on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FilteeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
