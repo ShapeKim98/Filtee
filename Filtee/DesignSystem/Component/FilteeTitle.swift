1de7ebe08e323d95d8ca427ece63839ee10cf65b
김도형 <shapekim98@gmail.com>
2025-05-14 09:52:31 +0900

    [feat] #1 FilteeTitle 구성

---
+++ Filtee/DesignSystem/Component/FilteeTitle.swift
@@ -0,0 +1,43 @@
+//
+//  FilteeTitle.swift
+//  Filtee
+//
+//  Created by 김도형 on 5/13/25.
+//
+
+import SwiftUI
+
+struct FilteeTitle<SubButton: View>: View {
+    private let title: String
+    private let subButton: SubButton
+    
+    init(
+        _ title: String,
+        subButton: @escaping () -> SubButton = { EmptyView() }
+    ) {
+        self.title = title
+        self.subButton = subButton()
+    }
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.pretendard(.body1(.bold)))
+                .foregroundStyle(.gray60)
+            
+            Spacer()
+            
+            subButton
+                .font(.pretendard(.body1(.medium)))
+                .foregroundStyle(.gray75)
+        }
+    }
+}
+
+#Preview {
+    FilteeTitle("Main Title")
+    
+    FilteeTitle("Main Title") {
+        Button("Sub Button", action: {})
+    }
+}
