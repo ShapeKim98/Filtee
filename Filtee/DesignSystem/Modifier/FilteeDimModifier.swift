ba23312e8c0e2ce746cb6f27dccfe4e0b3354c6d
김도형 <shapekim98@gmail.com>
2025-05-16 18:39:31 +0900

    [feat] #1 필티 딤 구성

---
+++ Filtee/DesignSystem/Modifier/FilteeDimModifier.swift
@@ -0,0 +1,29 @@
+//
+//  FilteeDimModifier.swift
+//  Filtee
+//
+//  Created by 김도형 on 5/16/25.
+//
+
+import SwiftUI
+
+private struct FilteeDimModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content.overlay {
+            LinearGradient(
+                stops: [
+                    Gradient.Stop(color: Color(red: 0.04, green: 0.04, blue: 0.04).opacity(0), location: 0.00),
+                    Gradient.Stop(color: Color(red: 0.04, green: 0.04, blue: 0.04), location: 1.00),
+                ],
+                startPoint: UnitPoint(x: 0.5, y: 0.25),
+                endPoint: UnitPoint(x: 0.5, y: 0.95)
+            )
+        }
+    }
+}
+
+extension View {
+    func filteeDim() -> some View {
+        modifier(FilteeDimModifier())
+    }
+}
