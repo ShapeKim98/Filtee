d394b0b9e1b224b26cd7d8b0f54c2ae87d237739
김도형 <shapekim98@gmail.com>
2025-05-22 16:02:32 +0900

    [feat] #5 FilterClient 구성

--- Filtee/Feature/Main/MainView.swift
+++ Filtee/Feature/Main/MainView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct MainView: View {
+    @Environment(\.userClient.todayAuthor)
+    private var userClientTodayAuthor
+    @Environment(\.filterClient.hotTrend)
+    private var filterClientHotTrend
+    @Environment(\.filterClient.todayFilter)
+    private var filterClientTodayFilter
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+#if DEBUG
 #Preview {
     MainView()
+        .environment(\.userClient, .testValue)
+        .environment(\.filterClient, .testValue)
 }
+#endif
