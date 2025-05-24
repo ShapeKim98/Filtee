cd9c9a8e8143343dcdae6daa11f2116ebba9097d
김도형 <shapekim98@gmail.com>
2025-05-24 11:32:55 +0900

    [feat] #5 오늘의 필터 섹션 구성

--- Filtee/Feature/Main/MainView.swift
+++ Filtee/Feature/Main/MainView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+import NukeUI
+
 struct MainView: View {
     @Environment(\.userClient.todayAuthor)
     private var userClientTodayAuthor
@@ -15,8 +17,114 @@ struct MainView: View {
     @Environment(\.filterClient.todayFilter)
     private var filterClientTodayFilter
     
+    @State
+    private var todayFilter: TodayFilterModel?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(content: content)
+            .ifLet(todayFilter?.filtered) { view, value in
+                view.background {
+                    VStack {
+                        LazyImage(url: URL(string: value)) { state in
+                            lazyImageTransform(state) { image in
+                                image.aspectRatio(contentMode: .fill)
+                                    .frame(height: 500)
+                            }
+                        }
+                        .filteeDim()
+                        
+                        Spacer()
+                    }
+                    .ignoresSafeArea(edges: .top)
+                }
+            }
+            .task(bodyTask)
+    }
+}
+
+// MARK: - Configure Views
+private extension MainView {
+    func content() -> some View {
+        VStack {
+            todayFilterSection
+            
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    var todayFilterSection: some View {
+        if let filter = todayFilter {
+            VStack(alignment: .leading, spacing: 20) {
+                
+                todyFilterButton(filter)
+                    .padding(.bottom, 147)
+                
+                todayFilterTitle(filter)
+                
+                Text(filter.description)
+                    .font(.pretendard(.caption1(.regular)))
+                    .multilineTextAlignment(.leading)
+                    .foregroundStyle(.secondary)
+            }
+            .padding(.horizontal, 20)
+            .frame(maxWidth: .infinity)
+            .background {
+                VisualEffect(style: .systemChromeMaterial)
+                    .mask(LinearGradient(
+                        colors: [.clear, .black],
+                        startPoint: UnitPoint(x: 0.5, y: 0.25),
+                        endPoint: UnitPoint(x: 0.5, y: 0.95)
+                    ))
+            }
+        }
+    }
+    
+    func todayFilterTitle(_ filter: TodayFilterModel) -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("오늘의 필터 소개")
+                .font(.pretendard(.body3(.medium)))
+                .foregroundStyle(.gray60)
+            
+            Group {
+                Text(filter.introduction)
+                
+                Text(filter.title)
+            }
+            .font(.mulgyeol(.title1))
+            .foregroundStyle(.gray30)
+        }
+    }
+    
+    func todyFilterButton(_ filter: TodayFilterModel) -> some View {
+        HStack {
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Text("사용해보기")
+                    .font(.pretendard(.caption1(.medium)))
+                    .foregroundStyle(.secondary)
+                    .padding(.horizontal, 10)
+                    .frame(height: 28)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(8)
+            }
+            .buttonStyle(.plain)
+        }
+    }
+}
+
+// MARK: - Fuctions
+private extension MainView {
+    @Sendable
+    func bodyTask() async {
+        do {
+            todayFilter = try await filterClientTodayFilter()
+        } catch {
+            print(error)
+        }
     }
 }
 
