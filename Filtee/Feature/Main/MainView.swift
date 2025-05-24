23b7709aa1fcedf8765e81411b73e0f50b14fd5c
김도형 <shapekim98@gmail.com>
2025-05-24 15:41:46 +0900

    [feat] #5 핫 트렌트 섹션 구성

--- Filtee/Feature/Main/MainView.swift
+++ Filtee/Feature/Main/MainView.swift
@@ -19,23 +19,32 @@ struct MainView: View {
     
     @State
     private var todayFilter: TodayFilterModel?
+    @State
+    private var hotTrends: [FilterModel] = []
+    @State
+    private var scrollOffset: CGFloat = 0
     
     var body: some View {
         ScrollView(content: content)
+            .coordinateSpace(name: "ScrollView")
+            .background {
+                VisualEffect(style: .systemChromeMaterial)
+                    .mask(LinearGradient(
+                        colors: [
+                            .black.opacity(scrollOffset / CGFloat(160)),
+                            .black.opacity(scrollOffset / CGFloat(160) + CGFloat(0.1)),
+                            .black,
+                            .black,
+                            .black
+                        ],
+                        startPoint: UnitPoint(x: 0.5, y: 0),
+                        endPoint: UnitPoint(x: 0.5, y: 1)
+                    ))
+                    .ignoresSafeArea()
+            }
             .ifLet(todayFilter?.filtered) { view, value in
                 view.background {
-                    VStack {
-                        LazyImage(url: URL(string: value)) { state in
-                            lazyImageTransform(state) { image in
-                                image.aspectRatio(contentMode: .fill)
-                                    .frame(height: 500)
-                            }
-                        }
-                        .filteeDim()
-                        
-                        Spacer()
-                    }
-                    .ignoresSafeArea(edges: .top)
+                    backgroundImage(url: value)
                 }
             }
             .task(bodyTask)
@@ -45,11 +54,14 @@ struct MainView: View {
 // MARK: - Configure Views
 private extension MainView {
     func content() -> some View {
-        VStack {
+        VStack(spacing: 0) {
             todayFilterSection
+            
+            hotTrendSection
             
             Spacer()
         }
+        .scrollOffset($scrollOffset, coordinateSpace: "ScrollView")
     }
     
     @ViewBuilder
@@ -70,12 +82,7 @@ private extension MainView {
             .padding(.horizontal, 20)
             .frame(maxWidth: .infinity)
             .background {
-                VisualEffect(style: .systemChromeMaterial)
-                    .mask(LinearGradient(
-                        colors: [.clear, .black],
-                        startPoint: UnitPoint(x: 0.5, y: 0.25),
-                        endPoint: UnitPoint(x: 0.5, y: 0.95)
-                    ))
+                
             }
         }
     }
@@ -114,6 +121,67 @@ private extension MainView {
             .buttonStyle(.plain)
         }
     }
+    
+    func backgroundImage(url: String) -> some View {
+        VStack {
+            LazyImage(url: URL(string: url)) { state in
+                lazyImageTransform(state) { image in
+                    image.aspectRatio(contentMode: .fill)
+                        .frame(height: 500)
+                }
+            }
+            .filteeDim()
+            
+            Spacer()
+        }
+        .ignoresSafeArea(edges: .top)
+    }
+    
+    @ViewBuilder
+    var hotTrendSection: some View {
+        VStack(spacing: 0) {
+            FilteeTitle("핫 트렌드")
+            
+            HotTrendList(filters: hotTrends) { filter in
+                hotTrendCell(filter)
+            }
+            .frame(height: 240)
+        }
+    }
+    
+    @ViewBuilder
+    func hotTrendCell(_ filter: FilterModel) -> some View {
+        LazyImage(url: URL(string: filter.filtered ?? "")) { state in
+            lazyImageTransform(state) { image in
+                image.aspectRatio(contentMode: .fill)
+            }
+        }
+        .frame(width: 200, height: 240)
+        .clipRectangle(8)
+        .clipped()
+        .overlay(alignment: .topLeading) {
+            Text(filter.title)
+                .font(.mulgyeol(.caption1))
+                .foregroundStyle(.gray30)
+                .padding(.top, 8)
+                .padding(.leading, 12)
+        }
+        .overlay(alignment: .bottomTrailing) {
+            HStack(spacing: 2) {
+                Image(filter.isLike ? .likeFill : .likeEmpty)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 16, height: 16)
+                
+                Text("\(filter.likeCount)")
+                    .font(.pretendard(.caption1(.semiBold)))
+                    .contentTransition(.numericText())
+            }
+            .foregroundStyle(.gray30)
+            .padding(.bottom, 8)
+            .padding(.trailing, 10)
+        }
+    }
 }
 
 // MARK: - Fuctions
@@ -121,7 +189,10 @@ private extension MainView {
     @Sendable
     func bodyTask() async {
         do {
-            todayFilter = try await filterClientTodayFilter()
+            async let todayFilter = filterClientTodayFilter()
+            async let hotTrends = filterClientHotTrend()
+            self.todayFilter = try await todayFilter
+            self.hotTrends = try await hotTrends
         } catch {
             print(error)
         }
