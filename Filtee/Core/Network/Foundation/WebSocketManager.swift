ea3cc53ccda57e72950cf2989fa2f4f20e270111
김도형 <shapekim98@gmail.com>
2025-07-09 17:54:26 +0900

    [fix] #14 소켓 통신 해제 누락 수정

--- Filtee/Core/Network/Foundation/WebSocketManager.swift
+++ Filtee/Core/Network/Foundation/WebSocketManager.swift
@@ -46,8 +46,18 @@ actor WebSocketManager<Message: ResponseDTO> {
     }
     
     func disconnect() async {
+        // 1. 모든 이벤트 핸들러 제거
+        socket?.removeAllHandlers()
+        
+        // 2. 소켓 연결 해제
         socket?.disconnect()
+        
+        // 3. Manager도 해제
+        manager?.disconnect()
+        
+        // 4. 참조 해제
         socket = nil
+        manager = nil
         isConnected = false
         subject = CurrentValueSubject<Message?, Error>(nil)
     }
