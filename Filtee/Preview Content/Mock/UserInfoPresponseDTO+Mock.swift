3fe4a9d9cd20066a171fd0b1b682af9a66752ba0
김도형 <shapekim98@gmail.com>
2025-07-10 19:05:00 +0900

    [feat] #16 검색 결과 api 구성

---
+++ Filtee/Preview Content/Mock/UserInfoPresponseDTO+Mock.swift
@@ -0,0 +1,24 @@
+//
+//  UserInfoPresponseDTO+Mock.swift
+//  Filtee
+//
+//  Created by 김도형 on 7/10/25.
+//
+
+import Foundation
+
+extension UserInfoResponseDTO {
+    static let mock = UserInfoResponseDTO(
+        userId: "6816ee1c6d1bff703149336f",
+        email: nil,
+        nick: "sesac",
+        name: nil,
+        introduction: "프로필 소개입니다.",
+        description: "김새싹",
+        profileImage: "/data/profiles/1712739634962.png",
+        phoneNum: nil,
+        hashTags: [
+            "#맑음"
+        ]
+    )
+}
